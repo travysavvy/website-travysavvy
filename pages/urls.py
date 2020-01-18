08d15e76201e1d4bd023b0ed5877a53aa27adb9e
jonsnow <abijithanikkuruthi@gmail.com>
2020-01-18 14:59:50 +0100

    added about

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from pages import views
 urlpatterns = [
     path('', views.index),
     path('contact/', views.contact),
+    path('about/', views.about),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def about(request):
+    return render(request, 'about.html', {})
+
 def contact(request):
     # form = MessageForm()
     # message_saved = False
